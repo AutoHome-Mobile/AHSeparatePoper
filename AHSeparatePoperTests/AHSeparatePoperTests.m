3ca48a0239bc2894d1ec071f984cf518aa3ea446
jun <junlinsong@gmail.com>
2014-04-30 11:11:42 +0800

    Initial Commit

---
+++ AHSeparatePoperTests/AHSeparatePoperTests.m
@@ -0,0 +1,34 @@
+//
+//  AHSeparatePoperTests.m
+//  AHSeparatePoperTests
+//
+//  Created by jun on 4/30/14.
+//  Copyright (c) 2014 Junkor. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AHSeparatePoperTests : XCTestCase
+
+@end
+
+@implementation AHSeparatePoperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
